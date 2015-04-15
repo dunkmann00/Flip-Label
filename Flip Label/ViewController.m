361558f64eb1269fde8ed2ec625bedbdff80253d
dunkmann00 <georgewaters@George-Waterss-MacBook-Pro.local>
2015-04-15 14:34:57 -0400

    Working Project using GWFlipLabel

--- Flip Label/ViewController.m
+++ Flip Label/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "GWFlipLabel.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet GWFlipLabel *flipLabel;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -17,11 +20,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.flipLabel.textAlignment = NSTextAlignmentCenter;
+    self.flipLabel.dynamicFontSize = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.flipLabel setTextWithFlipAnimationDuration:1.0 text:textField.text completion:^(BOOL finished) {
+        NSLog(@"Flip Complete!");
+    }];
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
